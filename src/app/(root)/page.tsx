1321bde6cfd31e97888c0fffde3d0c4a15b27e69
Kerstin Weibeler <kerstin.weibeler@eglo.com>
2025-04-20 20:00:52 +0200

    feat: add reusable ProductList component with optional title and item limit

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '../../../db/sample-data';
+
 const Homepage = () => {
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} limit={4} title="New Arrivals" />
+    </>
+  );
 };
 
 export default Homepage;
